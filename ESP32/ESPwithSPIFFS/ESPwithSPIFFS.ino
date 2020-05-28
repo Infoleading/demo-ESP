40bf8bd2b557ba31625da5c0e14b600c24819a1a
JianXing <simple_star@126.com>
2020-05-28 22:02:25 +0800

    modified SPIFFS testing

--- ESP32/ESPwithSPIFFS/ESPwithSPIFFS.ino
+++ ESP32/ESPwithSPIFFS/ESPwithSPIFFS.ino
@@ -7,23 +7,36 @@
  
 void setup() {
   Serial.begin(115200);
+  Serial.println("setup is running");
   
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  
-  File file = SPIFFS.open("/test.txt");
+
+  //  open file for writing
+  File file = SPIFFS.open("/test.txt", FILE_WRITE);
   if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (!file.print("This is content of file test.txt in SPIFFS.")){
+    Serial.println("Failed to write file");
+    return;
+  }
+  file.close();
+
+  // open file for reading
+  File file2 = SPIFFS.open("/test.txt");
+  if(!file2){
     Serial.println("Failed to open file for reading");
     return;
   }
-  
   Serial.println("File Content:");
-  while(file.available()){
-    Serial.write(file.read());
+  while(file2.available()){
+    Serial.write(file2.read());
   }
-  file.close();
+  file2.close();
 }
  
 void loop() {
